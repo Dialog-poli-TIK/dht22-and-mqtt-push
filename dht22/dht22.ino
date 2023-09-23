d42360a51dc4438506e470cac1674fb7b1cf6953
Moh Noor Al Azam <me@noorazam.my.id>
2023-09-23 11:05:05 +0700

    menambahkan wifi

--- dht22/dht22.ino
+++ dht22/dht22.ino
@@ -1,9 +1,32 @@
+#include <WiFi.h>
+
+#define SSID            "B6FEE3"
+#define PASSWORD        "fl4mb0y4n2022"
+
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
+  while(!Serial) {
+    delay(100);
+    }
 
+  Serial.println();
+  Serial.println("******************************************************");
+  Serial.print("Connecting to ");
+  Serial.println(SSID);
+  WiFi.begin(SSID, PASSWORD);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+    }
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  Serial.print("IP Address: ");
+  Serial.println(WiFi.localIP());
 }
