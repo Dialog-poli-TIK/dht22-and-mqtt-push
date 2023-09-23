5cc5b4a010bee008a39e8a4ca6b69d75f52eee64
Moh Noor Al Azam <me@noorazam.my.id>
2023-09-23 11:50:53 +0700

    dht22 ke mqtt

--- dht22/dht22.ino
+++ dht22/dht22.ino
@@ -1,7 +1,53 @@
 #include <WiFi.h>
+#include <PubSubClient.h>
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+#include <DHT_U.h>
 
 #define SSID            "B6FEE3"
 #define PASSWORD        "fl4mb0y4n2022"
+
+#define DHTPIN    4       // using D2 as 
+#define DHTTYPE   DHT22   // DHT22
+
+#define MQTT_SERVER     "202.154.5.67"
+#define MQTT_PORT       1883
+#define MQTT_USERNAME   "noor"
+#define MQTT_PASSWORD   "radnet"
+#define MQTT_OUTTOPIC   "noor/azam/test"
+
+WiFiClient espWiFi;
+PubSubClient client(espWiFi);
+
+DHT_Unified dht(DHTPIN, DHTTYPE);
+uint32_t delayMS;
+
+char temperature[10];
+char humidity[10];
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Create a random client ID
+    String clientId = "ESP32Client-";
+    clientId += String(random(0xffff), HEX);
+    // Attempt to connect
+    if (client.connect(clientId.c_str())) {
+      Serial.println("connected");
+      // Once connected, publish an announcement...
+      client.publish(MQTT_OUTTOPIC, "testing MQTT from DHT22");
+      // ... and resubscribe
+      //client.subscribe("inTopic");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
 
 void setup() {
   // put your setup code here, to run once:
@@ -23,10 +69,77 @@ void setup() {
   Serial.println("WiFi connected");
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
+
+  dht.begin();
+  Serial.println(F("DHTxx Unified Sensor Example"));
+  // Print temperature sensor details.
+  sensor_t sensor;
+  dht.temperature().getSensor(&sensor);
+  Serial.println(F("------------------------------------"));
+  Serial.println(F("Temperature Sensor"));
+  Serial.print  (F("Sensor Type: ")); Serial.println(sensor.name);
+  Serial.print  (F("Driver Ver:  ")); Serial.println(sensor.version);
+  Serial.print  (F("Unique ID:   ")); Serial.println(sensor.sensor_id);
+  Serial.print  (F("Max Value:   ")); Serial.print(sensor.max_value); Serial.println(F("°C"));
+  Serial.print  (F("Min Value:   ")); Serial.print(sensor.min_value); Serial.println(F("°C"));
+  Serial.print  (F("Resolution:  ")); Serial.print(sensor.resolution); Serial.println(F("°C"));
+  Serial.println(F("------------------------------------"));
+  // Print humidity sensor details.
+  dht.humidity().getSensor(&sensor);
+  Serial.println(F("Humidity Sensor"));
+  Serial.print  (F("Sensor Type: ")); Serial.println(sensor.name);
+  Serial.print  (F("Driver Ver:  ")); Serial.println(sensor.version);
+  Serial.print  (F("Unique ID:   ")); Serial.println(sensor.sensor_id);
+  Serial.print  (F("Max Value:   ")); Serial.print(sensor.max_value); Serial.println(F("%"));
+  Serial.print  (F("Min Value:   ")); Serial.print(sensor.min_value); Serial.println(F("%"));
+  Serial.print  (F("Resolution:  ")); Serial.print(sensor.resolution); Serial.println(F("%"));
+  Serial.println(F("------------------------------------"));
+  // Set delay between sensor readings based on sensor details.
+  delayMS = sensor.min_delay / 1000;
+
+  client.setServer(MQTT_SERVER, MQTT_PORT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   Serial.print("IP Address: ");
   Serial.println(WiFi.localIP());
+
+  sensors_event_t event;
+  dht.temperature().getEvent(&event);
+  if (isnan(event.temperature)) {
+    Serial.println(F("Error reading temperature!"));
+  }
+  else {
+    Serial.print(F("Temperature: "));
+    Serial.print(event.temperature);
+    sprintf(temperature, "%f", event.temperature);
+    Serial.println(F("°C"));
+  }
+  // Get humidity event and print its value.
+  dht.humidity().getEvent(&event);
+  if (isnan(event.relative_humidity)) {
+    Serial.println(F("Error reading humidity!"));
+  }
+  else {
+    Serial.print(F("Humidity: "));
+    Serial.print(event.relative_humidity);
+    sprintf(humidity, "%f", event.relative_humidity);
+    Serial.println(F("%"));
+  }
+
+  if (!client.connected()) {
+    reconnect();
+    }
+  else {
+    //randNumber = random(300);
+    char dataMQTT[80];
+
+    Serial.print("Publish at topic ");
+    Serial.print(MQTT_OUTTOPIC);
+    sprintf(dataMQTT, "temp: %s and hum: %s%% ", temperature, humidity);
+    Serial.println(dataMQTT);
+    client.publish(MQTT_OUTTOPIC, dataMQTT);
+  }
+  delay(5000);
 }
